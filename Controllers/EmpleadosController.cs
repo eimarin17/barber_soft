ea55ec23cd58e7a64c05f0ab87f18dcfc7581335
Einer Marin <emarin@dondoctor.com>
2022-05-29 01:04:42 -0500

    Se realiza HU001 - Administración de empleados

---
+++ Controllers/EmpleadosController.cs
@@ -0,0 +1,149 @@
+﻿using BarberSoft.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarberSoft.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private readonly barber_softContext _context;
+
+        public EmpleadosController(barber_softContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Empleados
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Empleados.ToListAsync());
+        }
+
+        // GET: Empleados/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleados = await _context.Empleados
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleados == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleados);
+        }
+
+        // GET: Empleados/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombres,Apellidos,Documento")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(empleados);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleados = await _context.Empleados.FindAsync(id);
+            if (empleados == null)
+            {
+                return NotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Nombres,Apellidos,Documento")] Empleados empleados)
+        {
+            if (id != empleados.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(empleados);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmpleadosExists(empleados.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleados = await _context.Empleados
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleados == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var empleados = await _context.Empleados.FindAsync(id);
+            _context.Empleados.Remove(empleados);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmpleadosExists(long id)
+        {
+            return _context.Empleados.Any(e => e.Id == id);
+        }
+    }
+}
